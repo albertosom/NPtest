94f7b26183681dadcd6ab4e8c9f4064a764075be
Albert Osom <osomalbert@gmail.com>
2024-07-21 01:15:25 -0700

    Update example.R

--- example.R
+++ example.R
@@ -18,11 +18,11 @@ Y<- sin(W*sign(W)) + epsilon
 
 ## our non-parametric test 
 lambdas<-seq(0,5e5,length.out=5)
-rr<-CondMean(Y,W,boot.samp=500,lambdas,lambda1=0.001,d=100,ncores=detectCores() - 1) 
+rr<-CondMean(Y,W,boot.samp=500,lambdas,d=100,ncores=detectCores() - 1) 
   
 
 
-## Testing null conditional average treatment effect 
+## Example 3 Testing null conditional average treatment effect 
 
 n=50
 A<-rbinom(n,size=1,prob=0.5)
@@ -36,5 +36,5 @@ bw <- npregbw(formula=Y~A+W,regtype="ll",bwmethod="cv.aic")
 npsigtest(bws=bw,index=c(1))
 
 lambdas<-seq(0,5e5,length.out=5)
-rr<-testnullcate(Y,W,A,boot.samp=500,lambdas,lambda1=0.001,d=100,ncores=detectCores() - 1)
+rr<-testnullcate(Y,W,A,boot.samp=500,lambdas,d=100,ncores=detectCores() - 1)
 
