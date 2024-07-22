0f6e02bed938cea3f50469de8e2ef954b9c1743e
Albert Osom <osomalbert@gmail.com>
2024-07-21 22:36:16 -0700

    Update example.R

--- example.R
+++ example.R
@@ -14,27 +14,33 @@ n=500
 A<-rbinom(n,size=1,prob=0.5)
 W<-rnorm(n,0,1)
 epsilon<-rnorm(n,0,1)
-Y<- sin(W*sign(W)) + epsilon
+#Y<- sin(W*sign(W)) + epsilon
+Y<- epsilon
 
 ## our non-parametric test 
-lambdas<-seq(0,5e5,length.out=5)
-rr<-CondMean(Y,W,boot.samp=500,lambdas,d=100,ncores=detectCores() - 1) 
-  
+lambdas<-seq(0,5e50,length.out=2)
+rr<-CondMean(Y,W,boot.samp=500,lambdas,d=100) 
+pvalue.aggreg<-aggreg.pvalue(pvals=rr$p.values,test.stats=rr$test.stats,samples.null=rr$null.dstr.lambdas)
+pvalue.cauchy<-CCT(pvals=rr$p.values, weights=NULL)
 
 
 ## Example 3 Testing null conditional average treatment effect 
 
-n=50
+n=100
 A<-rbinom(n,size=1,prob=0.5)
-W<-rnorm(n,0,1)
+#W<-rnorm(n,0,1)
+W<-runif(n,-1,1)
 epsilon<-rnorm(n,0,1)
-Y<- 0.5 + 0.5*W + 10*A + 1.6*W*A + epsilon
-#Y<-0.4 + 0.05*W + 0.8*A +epsilon
+#Y<- 0.5 + 0.5*W + 10*A + 1.6*W*A + epsilon
+Y<-0.4 + W + epsilon
+#Y<-0.4 + 0.05*W + 0.8*A
 
 ## approach by Racine (2006)
 bw <- npregbw(formula=Y~A+W,regtype="ll",bwmethod="cv.aic")
 npsigtest(bws=bw,index=c(1))
 
-lambdas<-seq(0,5e5,length.out=5)
-rr<-testnullcate(Y,W,A,boot.samp=500,lambdas,d=100,ncores=detectCores() - 1)
+lambdas<-seq(0,5e20,length.out=5)
+rr<-testnullcate(Y,W,A,boot.samp=500,lambdas,d=100)
+pvalue.aggreg<-aggreg.pvalue(pvals=rr$p.values,test.stats=rr$test.stats,samples.null=rr$null.dstr.lambdas)
+pvalue.cauchy<-CCT(pvals=rr$p.values, weights=NULL)
 
