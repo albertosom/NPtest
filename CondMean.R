86c89b7d5b042da954733d5a78de7baca515c180
Albert Osom <osomalbert@gmail.com>
2024-07-21 22:36:13 -0700

    Update CondMean.R

--- CondMean.R
+++ CondMean.R
@@ -1,18 +1,29 @@
 
-## conditional mean test 
-source("functionsNptest.R")
-library(MASS)
-library(parallel)
-library(mvtnorm)
 
-CondMean<-function(Y,X,boot.samp,lambdas,d=100,ncores=detectCores() - 1) {
+source("functionsNptest.R")
+
+#'  Test for conditional mean equal to the unconditional mean
+#'  
+#' @param Y a vector. The outcome variable. The length of Y equals to the number of observations.
+#' @param X covariate (1 dimensional for now)
+#' @param boot.samp Number of bootstrap samples
+#' @param d Number of basis
+#' @return A list containing necessary information to compute p-value of the test
+#' \item{test.stats}{Test statistics for different lambda values}
+#' \item{p.values}{P-values for the test for each value of lambda}
+#' \item {null.dstr.lambdas} {Multiplier bootstrap sample for approximating the distribution
+#'                             of the test statistic for different lambda values.}
+#' 
+#' @export
+#' 
+CondMean<-function(Y,X,boot.samp,lambdas,d=100) {
   
   n<-length(Y)
-  paths<-rmvnorm(boot.samp, sigma=diag(1,nrow=n))
   null.dist.matrix<-matrix(NA,nrow=boot.samp,ncol=length(lambdas))
   lamb.pval<-numeric()
   pvals.aggreg<-numeric()
   rr<-matrix(NA,ncol=4,nrow=length(lambdas))
+  test.sup<-numeric()
   
     for(k in 1:length(lambdas)){
       
@@ -20,7 +31,7 @@ CondMean<-function(Y,X,boot.samp,lambdas,d=100,ncores=detectCores() - 1) {
       lambda=lambdas[k]
       
       U<-SobBasis(X,d,n)
-      U<-U[,-1]
+      U<-U[,-1] # not include intercept
       psiu<-matrix(Y-mean(Y),ncol=1)
       S.theta<-(1/n)*(t(psiu)%*%U)
       Pen<-diag(c(1/(2*pi*seq(1,d,length.out=d))^(-4)))
@@ -33,60 +44,31 @@ CondMean<-function(Y,X,boot.samp,lambdas,d=100,ncores=detectCores() - 1) {
       V.hat<-h.hat%*%V%*%t(h.hat)
       
       U<-SobBasis(X,d,n)
-      U<-U[,-1]
+      U<-U[,-1] # not include intercept
       psiu<-matrix(Y-mean(Y),ncol=1)
       S.theta<-(1/n)*(t(psiu)%*%U)
       stat<-as.numeric(S.theta%*%t(h.hat))
       rr[k,]<-c(lambda,stat,rkhs.norm,V.hat)
       
       ### approximating asymptotic distribution under the null 
-      
-      stats <- mclapply(1:boot.samp, function(x0) {
-        
-        epsilon<-paths[x0,]
+    
+        for ( i in 1:boot.samp) {
+          
+        epsilon<-rnorm(n)
         psiu<-matrix(epsilon*(Y-mean(Y))-mean(epsilon)*out.reg,ncol=1)
-        U<-SobBasis(X,d,n)
-        U<-U[,-1]
         S.theta<-(1/n)*(t(psiu)%*%U)
-        Pen<-diag(c(1/(2*pi*seq(1,d,length.out=d))^(-4)))
-        
         h.hat<-S.theta%*%solve(Pen+lambda*V)
-        U<-SobBasis(X,d,n)
-        U<-U[,-1]
         psiu<-matrix(epsilon*(Y-mean(Y))-mean(epsilon)*out.reg1,ncol=1)
         S.theta<-(1/n)*(t(psiu)%*%U)
-        test.sup<-S.theta%*%t(h.hat)
+        test.sup[i]<-S.theta%*%t(h.hat)
         
-        return(test.sup)
-      } ,mc.cores = ncores)
-      
-      null.dist.matrix[,k]<- unlist(stats)
-      p.val<-mean(abs(unlist(stats)) > abs(stat),na.rm=T)
+        }
+       
+      null.dist.matrix[,k]<- test.sup
+      p.val<-mean(test.sup > abs(stat),na.rm=T)
       lamb.pval[k]<-p.val
+  
     }
-    
-    ## aggregating the test statistic
-    
-    for(b in 1:boot.samp ){
-      mu<-apply(null.dist.matrix[-b,],2,mean)
-      Ts.b<-null.dist.matrix[b,]
-      sigma<- sqrt(apply(null.dist.matrix[-b,],2,var))
-      pvals.aggreg[b]<-max(abs(( mu- Ts.b)/sigma))
-    }
-    
-    Q.b<-pvals.aggreg
-    
-    mu<-apply(null.dist.matrix,2,mean)
-    sigma<- sqrt(apply(null.dist.matrix,2,var))
-    Q.0<- max(abs(mu-rr[,2])/sigma)
-    
-    aggreg.pvalue<- (1/(boot.samp+1))*(1+ sum(ifelse(Q.b>Q.0,T,F)))
-    aggreg.Pvalue<-aggreg.pvalue     
-    
-    ## cauchy combination test
-    cct.pvalue<-CCT(lamb.pval,weights=rep(1/length(lambdas),length(lambdas)))
-    p.value.cauchy<-ifelse(cct.pvalue==0, "< 2.22e-16",cct.pvalue)
-    
-    return(list("aggregate"=paste0('p-value:',aggreg.Pvalue),"cauchy test"=paste0('p-value:',p.value.cauchy)))
+    return(list("test.stats"=rr[,2] , "p.values"=lamb.pval,"null.dstr.lambdas"=null.dist.matrix))
   }
   
